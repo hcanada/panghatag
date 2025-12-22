2157c08364581b922c64cea9097e58b207c0e722
xerea <harlybusinessmind@gmail.com>
2025-12-22 11:16:46 +0800

    refactor Homepage component structure for improved layout

--- components/dashboard/Homepage.tsx
+++ components/dashboard/Homepage.tsx
@@ -1,25 +1,32 @@
+import Link from "next/link";
 import Wrapper from "../layout/Wrapper";
 import { Button } from "../ui/button";
 
 export default function Homepage() {
   return (
-    <Wrapper className="max-w-xl my-20 md:my-28">
-      <main className="min-h-screen ">
-        <section className="text-center space-y-6 text-pretty">
-          <h1 className="font-bold text-4xl  leading-tight">
-            Give and Receive freely in your community
-          </h1>
-          <p>
-            Share items you no longer need with neighbors who will use them. No
-            buying, no selling, just good people helping good people. Start
-            giving today.
-          </p>
-          <div className="space-x-4">
-            <Button>Post Item</Button>
-            <Button variant={"secondary"}>Browse</Button>
-          </div>
-        </section>
-      </main>
-    </Wrapper>
+    <main>
+      <Wrapper className="max-w-xl my-20 md:my-28">
+        <main className="min-h-screen ">
+          <section className="text-center space-y-6 text-pretty">
+            <h1 className="font-bold text-4xl  leading-tight">
+              Give and Receive freely in your community
+            </h1>
+            <p>
+              Share items you no longer need with neighbors who will use them.
+              No buying, no selling, just good people helping good people. Start
+              giving today.
+            </p>
+            <div className="space-x-4">
+              <Link href="/upload">
+                <Button>Post Item</Button>
+              </Link>
+              <Link href="/items">
+                <Button variant={"secondary"}>Browse</Button>
+              </Link>
+            </div>
+          </section>
+        </main>
+      </Wrapper>
+    </main>
   );
 }
