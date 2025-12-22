1ecabfd4328e1b9d8c795c530a48bded3fd01a5c
xerea <harlybusinessmind@gmail.com>
2025-12-22 11:16:57 +0800

    add NavBar component for navigation with links to Home, Browse Items, Post Items, Log in, and Signup

---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export default function NavBar() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-white/95 backdrop-blur-sm">
+      <nav className="w-full flex h-16 justify-between items-center px-4 md:px-8 lg:px-16">
+        <Link href="/" className="text-green-500 font-bold text-xl">
+          Hatag{" "}
+        </Link>
+
+        <div className="hidden md:flex">
+          <Link href="/">
+            <Button variant={"link"}>Home</Button>
+          </Link>
+          <Link href="/items">
+            <Button variant={"link"}>Browse Items</Button>
+          </Link>
+          <Link href="/upload">
+            <Button variant={"link"}>Post Items</Button>
+          </Link>
+        </div>
+        <div className="flex gap-x-2">
+          <Link href="/login">
+            <Button>Log in</Button>
+          </Link>
+          <Link href="/signup">
+            <Button variant={"secondary"}>Signup</Button>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
