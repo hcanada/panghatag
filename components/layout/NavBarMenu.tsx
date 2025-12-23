a935af5ecd4afb856a2d1026805f88b857eb78e2
xerea <harlybusinessmind@gmail.com>
2025-12-23 16:54:14 +0800

    feat: add NavBarMenu component with dropdown for user settings and sign out

---
+++ components/layout/NavBarMenu.tsx
@@ -0,0 +1,53 @@
+"use client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LogOut, Settings, SunMoon, UserRoundPen } from "lucide-react";
+import { useTheme } from "next-themes";
+import { createClient } from "@/lib/supabase/client";
+import { useRouter } from "next/navigation";
+
+export default function NavBarMenu() {
+  const supabase = createClient();
+  const route = useRouter();
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) return console.log("Error signing out:", error.message);
+    route.refresh();
+  };
+  const { setTheme } = useTheme();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="md:size-10">
+          <AvatarImage src="https://github.com/hcanada.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="start">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <UserRoundPen /> Profile
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            <SunMoon /> Dark mode
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Settings /> Settings
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuItem onClick={handleSignOut}>
+          <LogOut />
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
