ec4b5f30844057361635cd2164b0a7257fe8d49d
xerea <harlybusinessmind@gmail.com>
2025-12-23 16:54:33 +0800

    feat: enhance NavBar with user authentication and conditional rendering

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
+import NavBarMenu from "./NavBarMenu";
+import { createClient } from "@/lib/supabase/server";
 
-export default function NavBar() {
+export default async function NavBar() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await (await supabase).auth.getUser();
+  console.log("Current user:", user);
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-white/95 backdrop-blur-sm">
+    <header className="sticky top-0 z-50 w-full border-b bg-white/95 dark:bg-[#18191A] backdrop-blur-sm">
       <nav className="w-full flex h-16 justify-between items-center px-4 md:px-8 lg:px-16">
         <Link href="/" className="text-green-500 font-bold text-xl">
           Hatag{" "}
@@ -20,14 +28,19 @@ export default function NavBar() {
             <Button variant={"link"}>Post Items</Button>
           </Link>
         </div>
-        <div className="flex gap-x-2">
-          <Link href="/login">
-            <Button>Log in</Button>
-          </Link>
-          <Link href="/signup">
-            <Button variant={"secondary"}>Signup</Button>
-          </Link>
-        </div>
+
+        {user ? (
+          <NavBarMenu />
+        ) : (
+          <div className="flex gap-x-2">
+            <Link href="/login">
+              <Button>Log in</Button>
+            </Link>
+            <Link href="/signup">
+              <Button variant={"secondary"}>Signup</Button>
+            </Link>
+          </div>
+        )}
       </nav>
     </header>
   );
