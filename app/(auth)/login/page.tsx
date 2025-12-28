bc2e5556c34ab18e9b074a3a6616ba6801a6b959
xerea <harlybusinessmind@gmail.com>
2025-12-28 20:44:11 +0800

    add suspense for useSearchParams to work

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,13 @@
 import { LoginForm } from "@/components/auth/login-form";
+import { Suspense } from "react";
 
 export default function Login() {
   return (
     <div className="bg-muted flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <Suspense fallback={null}>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   );
