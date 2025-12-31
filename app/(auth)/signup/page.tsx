ef99977bcb9c470bdbaf28dbe425a17bec60e256
xerea <harlybusinessmind@gmail.com>
2025-12-31 10:13:12 +0800

    add suspense fo signup form for redirect and useSearchParams

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,10 +1,13 @@
 import { SignupForm } from "@/components/auth/signup-form";
+import { Suspense } from "react";
 
 export default function Signup() {
   return (
     <div className="bg-muted flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <SignupForm />
+        <Suspense fallback={null}>
+          <SignupForm />
+        </Suspense>
       </div>
     </div>
   );
