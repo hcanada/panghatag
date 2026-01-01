8125e472c1672a23804b05ab9601c4a356565713
xerea <harlybusinessmind@gmail.com>
2026-01-01 10:03:41 +0800

    add lib getCurrentUser

---
+++ lib/authserver.ts
@@ -0,0 +1,17 @@
+// lib/auth.ts
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+
+export async function getCurrentUser() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
+  return user;
+}
