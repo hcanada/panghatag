ae2e860adfcb9e6f31df45a14724fd134d49503c
xerea <harlybusinessmind@gmail.com>
2026-01-02 10:50:19 +0800

    refactor: use ItemList in the profile page

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -2,8 +2,7 @@ import Wrapper from "@/components/layout/Wrapper";
 import { getCurrentUser } from "@/lib/authserver";
 import { formatMonthYear } from "@/lib/date";
 import { createClient } from "@/lib/supabase/server";
-import Link from "next/link";
-import Image from "next/image";
+import ItemsList from "@/components/items/items-list";
 
 export default async function Profile() {
   const supabase = await createClient();
@@ -35,35 +34,8 @@ export default async function Profile() {
           <h1 className="font-semibold text-xl md:text-3xl my-4 md:my-6">
             Items shared
           </h1>
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {itemsData.map((item) => (
-              <div key={item.id}>
-                <Link
-                  href={`/items/${item.id}`}
-                  className="space-y-2 flex flex-col hover:underline hover:underline-offset-2 "
-                >
-                  <div className="relative h-48 md:h-64 w-full">
-                    <Image
-                      src={item.images[0]}
-                      alt={item.title}
-                      fill
-                      className="rounded-md object-cover"
-                    />
-                    {/* <p className="absolute text-sm  bottom-0 left-0 pl-2 pb-2">
-                  {item.status}
-                </p> */}
-                  </div>
-                  <h2 className="font-bold text-xl">{item.title}</h2>
-                  <p className="text-sm text-shadow-muted-foreground font-medium ">
-                    {item.description}
-                  </p>
-                  <footer className="flex flex-col text-sm text-muted-foreground mt-auto">
-                    <p>{item.city}</p>
-                    <p>{item.status}</p>
-                  </footer>
-                </Link>
-              </div>
-            ))}
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
+            <ItemsList data={itemsData} />
           </div>
         </section>
       </Wrapper>
