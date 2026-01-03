ee2fb0ae6255f444408e1ce72674bffb602666d8
xerea <harlybusinessmind@gmail.com>
2026-01-03 10:43:05 +0800

    get current user client

---
+++ lib/auth/get-user-client.ts
@@ -0,0 +1,22 @@
+// hooks/use-current-user.ts
+"use client";
+
+import { createClient } from "@/lib/supabase/client";
+import { User } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
+export function useCurrentUser() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const supabase = createClient();
+
+    supabase.auth.getUser().then(({ data }) => {
+      setUser(data.user);
+      setLoading(false);
+    });
+  }, []);
+
+  return { user, loading };
+}
