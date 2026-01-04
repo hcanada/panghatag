55b6cfd0f4691bdbb8f89db09e164f07407cefdf
xerea <harlybusinessmind@gmail.com>
2026-01-04 19:25:48 +0800

    remove loading

--- lib/auth/get-user-client.ts
+++ lib/auth/get-user-client.ts
@@ -7,16 +7,14 @@ import { useEffect, useState } from "react";
 
 export function useCurrentUser() {
   const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const supabase = createClient();
 
     supabase.auth.getUser().then(({ data }) => {
       setUser(data.user);
-      setLoading(false);
     });
   }, []);
 
-  return { user, loading };
+  return user;
 }
