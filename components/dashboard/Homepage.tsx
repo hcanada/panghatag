d178a7c954de7c397a9c9caffdad8b376df22fb0
xerea <harlybusinessmind@gmail.com>
2026-01-04 19:27:41 +0800

    change text

--- components/dashboard/Homepage.tsx
+++ components/dashboard/Homepage.tsx
@@ -6,26 +6,23 @@ export default function Homepage() {
   return (
     <main>
       <Wrapper className="max-w-xl my-20 md:my-28">
-        <main>
-          <section className="text-center space-y-6 text-pretty">
-            <h1 className="font-bold text-4xl  leading-tight">
-              Give and Receive freely in your community
-            </h1>
-            <p>
-              Share items you no longer need with neighbors who will use them.
-              No buying, no selling, just good people helping good people. Start
-              giving today.
-            </p>
-            <div className="space-x-4">
-              <Link href="/upload">
-                <Button>Post Item</Button>
-              </Link>
-              <Link href="/items">
-                <Button variant={"secondary"}>Browse</Button>
-              </Link>
-            </div>
-          </section>
-        </main>
+        <section className="text-center space-y-6 text-pretty">
+          <h1 className="font-bold text-4xl  leading-tight">
+            Give and Receive freely in your community
+          </h1>
+          <p className="text-muted-foreground">
+            Join thousands of neighbors sharing items they no longer need.
+            Reduce waste, help others, and strengthen your local community.
+          </p>
+          <div className="space-x-4">
+            <Link href="/upload">
+              <Button>Post Item</Button>
+            </Link>
+            <Link href="/items">
+              <Button variant={"secondary"}>Browse</Button>
+            </Link>
+          </div>
+        </section>
       </Wrapper>
     </main>
   );
