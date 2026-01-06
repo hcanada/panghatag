8d56d9e4f7a4e7d41df350f2db668ac53fdd3355
xerea <harlybusinessmind@gmail.com>
2026-01-06 19:11:18 +0800

    `images.domains` is deprecated in favor of `images.remotePatterns`.  update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["wvzctetqhvqohlxkhpxv.supabase.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wvzctetqhvqohlxkhpxv.supabase.co",
+        port: "", // leave empty unless using a custom port
+        pathname: "/storage/v1/object/public/**", // optional, restrict to your bucket
+      },
+    ],
   },
 };
 
