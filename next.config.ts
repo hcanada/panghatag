103fa3d8d256fba4d8c5262e9e06b51e2e92567f
xerea <harlybusinessmind@gmail.com>
2026-01-06 19:02:22 +0800

    add supabase domain for image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["wvzctetqhvqohlxkhpxv.supabase.co"],
+  },
 };
 
 export default nextConfig;
