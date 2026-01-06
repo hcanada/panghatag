0d04bf3c753dabaf976abe9500749faff27280a3
xerea <harlybusinessmind@gmail.com>
2026-01-06 19:03:10 +0800

    add types export for item

---
+++ components/types/item.ts
@@ -0,0 +1,25 @@
+export type ItemStatus = "available" | "reserved" | "claimed";
+
+export interface Profile {
+  id: string;
+  username: string;
+  verified: boolean;
+  barangay: string | null;
+  bio: string | null;
+  city: string | null;
+  created_at: string;
+}
+
+export interface Item {
+  id: number;
+  user_id: string;
+  title: string;
+  description: string;
+  category: string;
+  images: string;
+  profiles: Profile;
+  barangay: string;
+  created_at: Date;
+  city: string;
+  status: ItemStatus;
+}
