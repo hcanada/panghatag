afcec6b07b6396e2c0f5f92c9e7a34c417b1112e
xerea <harlybusinessmind@gmail.com>
2026-01-09 13:58:12 +0800

    refactor: status to action feat:  add mark as claimed and cancel reservation;

--- app/api/requestclaim/route.ts
+++ app/api/requestclaim/route.ts
@@ -4,40 +4,81 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   // Handle POST request
   const supabase = await createClient();
-  const { id, item_id, status } = await req.json();
+  const { id, item_id, action } = await req.json();
 
-  if (status === "approved") {
-    const { error } = await supabase.rpc("approve_claim_request", {
-      p_claim_id: id,
-      p_item_id: item_id,
-    });
+  switch (action) {
+    case "approved":
+      const { error: approveError } = await supabase.rpc(
+        "approve_claim_request",
+        {
+          p_claim_id: id,
+          p_item_id: item_id,
+        }
+      );
 
-    if (error) {
-      console.error(error);
-      return NextResponse.json({ error }, { status: 500 });
-    }
+      if (approveError) {
+        console.error(approveError);
+        return NextResponse.json({ approveError }, { status: 500 });
+      }
 
-    return NextResponse.json(
-      { message: "success", action: "approved" },
-      { status: 200 }
-    );
-  } else if (status === "rejected") {
-    const { error } = await supabase
-      .from("claims")
-      .update({ status, updated_at: new Date() })
-      .eq("id", id)
-      .eq("status", "pending")
-      .select();
+      return NextResponse.json(
+        { message: "success", action: "approved" },
+        { status: 200 }
+      );
 
-    if (error) {
-      console.error(error);
-      return NextResponse.json({ error }, { status: 500 });
-    }
-    return NextResponse.json(
-      { message: "success", action: "rejected" },
-      { status: 200 }
-    );
-  } else {
-    return NextResponse.json({ error: "Unexpected Error" }, { status: 500 });
+    case "rejected":
+      const { error: rejectError } = await supabase
+        .from("claims")
+        .update({ status: action, updated_at: new Date() })
+        .eq("id", id)
+        .eq("status", "pending");
+
+      if (rejectError) {
+        console.error(rejectError);
+        return NextResponse.json({ rejectError }, { status: 500 });
+      }
+
+      return NextResponse.json(
+        { message: "success", action: "rejected" },
+        { status: 200 }
+      );
+
+    case "claimed":
+      const { error: claimedError } = await supabase
+        .from("items")
+        .update({ status: "claimed", updated_at: new Date() })
+        .eq("id", item_id)
+        .eq("status", "reserved");
+
+      if (claimedError) {
+        console.error(claimedError);
+        return NextResponse.json({ claimedError }, { status: 500 });
+      }
+
+      return NextResponse.json(
+        { message: "success", action: "claimed" },
+        { status: 200 }
+      );
+    case "cancel":
+      const { error: cancelError } = await supabase.rpc(
+        "mark_claim_cancel_request",
+        {
+          p_claim_id: id,
+          p_item_id: item_id,
+        }
+      );
+
+      if (cancelError) {
+        console.error(cancelError);
+        return NextResponse.json({ cancelError }, { status: 500 });
+      }
+
+      return NextResponse.json(
+        { message: "success", action: "cancel" },
+        { status: 200 }
+      );
+
+    default:
+      return NextResponse.json({ error: "Unexpected Error" }, { status: 500 });
   }
 }
